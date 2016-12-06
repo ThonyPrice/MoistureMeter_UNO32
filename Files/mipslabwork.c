d0d69f0483789ac1dc68eb4c6b56b88ce5452d27
Thony Price <thonyprice@gmail.com>
2016-12-06 13:18:24 +0100

    Cleanup of mipslabwork

--- Files/mipslabwork.c
+++ Files/mipslabwork.c
@@ -1,13 +1,7 @@
 /* mipslabwork.c
    Original file written 2015 by F Lundevall
    Modified by Thony Price for course IS1500, KTH 
-   Last revision: 2016-11-25          
-
-   Current to do:
-   
-    Read analog input from moisture sensor at port A1 by;
-        Set the corresponding bits in the AD1PCFG register = 0
-        Set the corresponding TRIS bit = 1 (input). 
+   Last revision: 2016-12-06          
 
 */
 
@@ -15,22 +9,27 @@
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
 #define TMR2PERIOD ((80000000 / 256) / 10)  // Set time-out-period to 10 ms
-#if TMR2PERIOD > 0xffff
+#if TMR2PERIOD > 0xffff                     // Fail-safe TMR2PERIOD size
 #error "Timer period is too big."
 #endif
-int timeoutcount = 0;
-
-// This code will no longer be used(?)
-int mytime = 0x5957;
-volatile int* myTRISE;
-volatile int* myPORTE;
-volatile int* myTRISA;
-// Declare variable where which later should refer to an adress
-volatile int* add;
+int mytime = 0x5957;  // Delete?
+int timeoutcount = 0; // Counter it interrupt timeouts 
+volatile int* add;    // Declare variable which refers to adress
+int* value;           // [Text]
 
 /* Interrupt Service Routine */
 void user_isr( void ) {
   
+  add = (volatile int*) 0xbf809070;
+  display_debug(add);          // Display what's read at A0
+  value = *add;
+  
+  if (value > 260){
+    PORTE = 3;
+  }
+  if (value < 260){
+    PORTE = 0;
+  }
   // check which flag's up
   int t2_flag   = IFS(0) & 256; // Get status of T2 flag
   int int1_flag = IFS(0) & 128; // Get status of INT1 flag  
@@ -63,8 +62,7 @@ void user_isr( void ) {
       // time2string( textstring, mytime );
       // display_string( 3, textstring );
       // display_update();
-      add = (volatile int*) 0xbf809070;
-      display_debug(add);          // Display what's read at A0
+
       // display_image(96, icon);
       tick( &mytime );
       timeoutcount = 0;
@@ -72,7 +70,7 @@ void user_isr( void ) {
   }
   
   if (int1_flag == 128){
-    (*myPORTE)++;
+    // PORTE++;
     IFS(0) ^= 128;
   }
 }
@@ -80,6 +78,7 @@ void user_isr( void ) {
 /* Initialize */
 void labinit( void )
 { 
+                                // Initialize ADC
   AD1CON1CLR= 0x00008000;       // Turn off the ADC before operating on it
   AD1PCFG   = 0x0000;           // Enable all analog inputs
   TRISASET  = 4;                // Set coresponding TRISbit (A2)
@@ -100,39 +99,24 @@ void labinit( void )
   
   AD1CON1SET= 0x00008000;       // Turn on the ADC
   AD1CON1SET= 0x00000004;       // Enable auto sampling
+
+                                // Inititalize timer
+  T2CON   = 0x8070;             // Set prescale to 1:256
+  PR2     = TMR2PERIOD;         // Initialize the timer
+  TMR2    = 0;                  // Reset timer 2
+  T2CONSET = 0x8000;            // Start the timer 
   
-  // AD1CHS  |= 0x00030000;    // Choose A1 for analog to digital conversion(?)
-  // myTRISA = (volatile int*) 0xbf886000;
-  // *myTRISA |= 0x2;
-  // TRISASET = 2;             // Set A1 as input instead of output (default)
-  // PORTA  &= 0xffffffff;     // Set 
-   
-  myTRISE   = (volatile int*) 0xbf886100;
-  myPORTE   = (volatile int*) 0xbf886110;
-  *myTRISE &= ~0x00;
-  PORTE = 0;
-  
-  int bitmask = (0x3f << 5);
-  TRISD |= bitmask;
-  
-  T2CON   = 0x8070;                   // Set prescale to 1:256
-  PR2     = TMR2PERIOD;               // Initialize the timer
-  TMR2    = 0;                        // Reset timer 2
-  T2CONSET = 0x8000;                  // Start the timer 
-  
-  IPC(2) = 4;                         // Interrupt priority control for TMR2
-  IPC(1) = 28;                        // IPC for 
-  IFS(0) = 0;                         // Reset all event flags (crude!)
-  
+  IPC(2) = 4;                   // Interrupt priority control for TMR2
+  IPC(1) = 28;                  // Set IPC1 to same priority
+  IFS(0) = 0;                   // Reset all event flags 
   IEC(0) = 384;
-  enable_interrupts();                // Call funtion in labwork.S
+  enable_interrupts();          // Call funtion in labwork.S
 
 }
 
 /* This function is called repetitively from the main program */
 void labwork( void ) {  
-  // add = (volatile int*) 0xbf809070;
-  // display_debug(add);          // Display what's read at A0
+  
   int foo = getsw();
   foo &= 0b0001;
   if (getsw() == 1){
