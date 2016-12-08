b9b93d85f82bf8dca470d10e0eefc00e4f6ed994
Thony Price <thonyprice@gmail.com>
2016-12-08 17:36:35 +0100

    Final version 1.0

--- Files/time4io.c
+++ Files/time4io.c
@@ -28,32 +28,33 @@ int getLeds(int value, int inAirVal, int waterVal, int plantMode){
   int span_in_range = inAirVal - waterVal;
   int measuredValue = value - waterVal;
   float prop = 1 - ((float)measuredValue/(float)span_in_range);  
-  // Adjust for plantMode (cactuses doesn't need as much water)
+  // Adjust for plantMode (flowers are more sensitive than cactuses :) )
   if (plantMode == 2){
-      prop = prop * 0.9;
+    prop = prop * 0.7;
   }
   // Raise flag if below a critical pecentage (plantMode dependent)
-  if ((prop <= ((float)2/(float)8 && plantMode == 1)) ||
-      (prop <= ((float)2/(float)8 && plantMode == 2)) ){
-       IFS(1) = 1;
+  if (prop <= (float)2/(float)8 && plantMode == 1){
+    IFS(1) = 1;
+  } if (prop <= (float)2/(float)8 && plantMode == 2){
+    IFS(1) = 1;
   }
   // Return amount of LEDs to light up by register PORTE 
   // Note that high proportions return low values because 
   // the higher value, the more dry the soil is
   if (prop > ((float)7/(float)8)){     
-      return 255; }
+    return 255;   }
   if (prop <= ((float)7/(float)8) && prop > ((float)6/(float)8)){
-      return 127; }
+    return 127;   }
   if (prop <= ((float)6/(float)8) && prop > ((float)5/(float)8)){
-      return 63;  }
+    return 63;   }
   if (prop <= ((float)5/(float)8) && prop > ((float)4/(float)8)){
-      return 31;  }
+    return 31;  }
   if (prop <= ((float)4/(float)8) && prop > ((float)3/(float)8)){
-      return 15;  }
+    return 15;  }
   if (prop <= ((float)3/(float)8) && prop > ((float)2/(float)8)){
-      return 7;   }
+    return 7;  }
   if (prop <= ((float)2/(float)8) && prop > ((float)1/(float)8)){
-      return 3;   }
+    return 3; }
   if (prop <= ((float)1/(float)8)){
-      return 1;   }
+    return 1; }
 }
